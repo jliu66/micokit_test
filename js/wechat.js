8b7917c5b53337a01c953201b7a3175c0179619c
jenny <jliu@u-gen.net>
2015-10-15 16:07:48 +0800

    td

--- js/wechat.js
+++ js/wechat.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
             // var d = new Date();
             // var timestamp = d.getTime();
             var signature = signWechat(data);
+            alert(signature);
             wx.config({
                 //debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: data.appId, // 必填，公众号的唯一标识
