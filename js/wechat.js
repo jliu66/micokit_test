fee34332e888db4713549b54fccf5fb9712553c7
jenny <jliu@u-gen.net>
2015-10-15 18:03:43 +0800

    545

--- js/wechat.js
+++ js/wechat.js
@@ -41,7 +41,10 @@ $(document).ready(function() {
                 wx.invoke('openWXDeviceLib', {}, function (res) {
                     alert("wx.openWXDeviceLib "+JSON.stringify(res));
                 });
-                wx.invoke('getWXDeviceTicket', {
+                
+
+                $('#share').on("click", function() {
+                    wx.invoke('getWXDeviceTicket', {
                     'deviceId': 'C68F841A219D452F1411EFD95F22538EEBB4C504E90EF3166F20DE7B5A57F792',
                     'type': '2'
                 }, function(res) {
@@ -52,29 +55,28 @@ $(document).ready(function() {
                         console.log(ticket);
                     }
                 });
-
-                $('#share').on("click", function() {
+                    
                     alert('share');
-                    wx.onMenuShareAppMessage({
-                        title: '互联网之子',
-                        desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-                        link: 'http://movie.douban.com/subject/25785114/',
-                        imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-                        trigger: function(res) {
-                            // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                            alert('用户点击发送给朋友');
-                        },
-                        success: function(res) {
-                            alert('已分享');
-                        },
-                        cancel: function(res) {
-                            alert('已取消');
-                        },
-                        fail: function(res) {
-                            alert(JSON.stringify(res));
-                        }
-                    });
-                    alert('已注册获取“发送给朋友”状态事件');
+                    // wx.onMenuShareAppMessage({
+                    //     title: '互联网之子',
+                    //     desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+                    //     link: 'http://movie.douban.com/subject/25785114/',
+                    //     imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+                    //     trigger: function(res) {
+                    //         // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+                    //         alert('用户点击发送给朋友');
+                    //     },
+                    //     success: function(res) {
+                    //         alert('已分享');
+                    //     },
+                    //     cancel: function(res) {
+                    //         alert('已取消');
+                    //     },
+                    //     fail: function(res) {
+                    //         alert(JSON.stringify(res));
+                    //     }
+                    // });
+                    // alert('已注册获取“发送给朋友”状态事件');
                 })
             })
 
