0e5ee154344c9d1f756bcd369427522f83209468
jenny <jliu@u-gen.net>
2015-10-15 16:34:37 +0800

    fff

--- js/wechat.js
+++ js/wechat.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
                     'openWXDeviceLib',
                     'closeWXDeviceLib',
                     'getWXDeviceTicket',
+                    'onMenuShareAppMessage'
                 ]
             });
 
